4df44f23c6eaf2f33f57300b5ceb7dd717782cea
Iceman <okamura@qoncept.co.jp>
2023-01-11 14:52:04 +0900

    Use wasi supported SwiftTypeReader

--- Sources/CodableToTypeScript/Generator/PackageGenerator.swift
+++ Sources/CodableToTypeScript/Generator/PackageGenerator.swift
@@ -2,6 +2,7 @@ import Foundation
 import SwiftTypeReader
 import TypeScriptAST
 
+#if !os(WASI)
 public final class PackageGenerator {
     public init(
         context: SwiftTypeReader.Context,
@@ -114,3 +115,4 @@ public final class PackageGenerator {
         }
     }
 }
+#endif
